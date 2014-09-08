151237b7fe924ddad3ee2c2f02c835e18df53e13
herrIsar <sha.he@me.com>
2014-09-08 16:54:11 +0200

    Third Commit

---
+++ Git TestTests/SHAThirdTest.h
@@ -0,0 +1,13 @@
+//
+//  SHAThirdTest.h
+//  Git Test
+//
+//  Created by Sha He on 08.09.14.
+//  Copyright (c) 2014 Sha He. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SHAThirdTest : NSObject
+
+@end
---
+++ Git TestTests/SHAThirdTest.m
@@ -0,0 +1,13 @@
+//
+//  SHAThirdTest.m
+//  Git Test
+//
+//  Created by Sha He on 08.09.14.
+//  Copyright (c) 2014 Sha He. All rights reserved.
+//
+
+#import "SHAThirdTest.h"
+
+@implementation SHAThirdTest
+
+@end
