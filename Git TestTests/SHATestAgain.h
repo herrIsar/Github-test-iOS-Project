26d4ebcc1e7c502e8e66b9a7e851482e49e42ce6
herrIsar <sha.he@me.com>
2014-09-08 16:51:19 +0200

    added testAgainClass

---
+++ Git TestTests/SHATestAgain.h
@@ -0,0 +1,13 @@
+//
+//  SHATestAgain.h
+//  Git Test
+//
+//  Created by Sha He on 08.09.14.
+//  Copyright (c) 2014 Sha He. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SHATestAgain : NSObject
+
+@end
---
+++ Git TestTests/SHATestAgain.m
@@ -0,0 +1,13 @@
+//
+//  SHATestAgain.m
+//  Git Test
+//
+//  Created by Sha He on 08.09.14.
+//  Copyright (c) 2014 Sha He. All rights reserved.
+//
+
+#import "SHATestAgain.h"
+
+@implementation SHATestAgain
+
+@end
